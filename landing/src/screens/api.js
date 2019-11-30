3d6233b64fa623e28607d6fed27aab3e3896cca4
Adam Dill <adamtdill@gmail.com>
2019-11-30 12:47:58 -0500

    Removing the landing page and prompting the user to visit games.adamdill.com/api instead.

--- landing/src/screens/api.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class ApiScreen extends React.Component {
-    render() {
-        return (
-            <p>Service API is to be written.</p>
-        );
-    }
-}
